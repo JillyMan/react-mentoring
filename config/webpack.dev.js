c4cdc0c4504769c78923c0f624d01f342ab44d60
Artsiom_Dubinevich <artsiom_dubinevich@epam.com>
2021-09-20 16:10:29 +0300

    move webpack config to ./config folder.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,34 +2,35 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
+const dirname = path.resolve(__dirname, '../src');
+
 module.exports = {
-    entry: './src/index.tsx',
-    mode: process.env.NODE_ENV,
-    devtool: process.env.NODE_ENV == 'development' ? 'inline-source-map' : undefined,
+    mode: 'development',
+    context: dirname,
+    entry: './index.tsx',
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, '../dist'),
         filename: "[name].[contenthash].js",
     },
     plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({ template: path.join(__dirname, "src", "index.html") })
+        new HtmlWebpackPlugin({ template: "./index.html" })
     ],
-    devServer: process.env.NODE_ENV === 'development' ? {
+    devtool: 'inline-source-map',
+    devServer: {
         port: 3001,
-    } : undefined,
+    },
     optimization: {
         splitChunks: {
             chunks: 'all',
         }
     },
     resolve: {
-        modules: [path.resolve(__dirname, './src'), 'node_modules'],
+        modules: [
+            path.resolve(dirname),
+            'node_modules',
+        ],
         extensions: ['.ts', '.tsx', '.js', '.jsx', 'json']
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
     },
     module: {
         rules: [
