e6caebe9238e1fd1bbe3700a07ceffa7241f7365
artsiom_dubinevich <artsiom_dubinevich@epam.com>
2021-10-08 17:52:24 +0300

    fix warning for modal window

--- src/modules/configuration-movie/components/configuration-movie.tsx
+++ src/modules/configuration-movie/components/configuration-movie.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ForwardedRef, forwardRef, useState } from 'react';
 import DatePicker from '@mui/lab/DatePicker';
 import SendIcon from '@mui/icons-material/Send';
 import {
@@ -63,176 +63,183 @@ const requiredFildFilled = (
     return filds.every((field) => !!movieConfig[field as keyof MovieConfig]);
 };
 
-export const ConfigurationMovie = ({
-    configTitle,
-    movieConfig,
-    avaliableGenres,
-    onSubmitClick,
-}: Props) => {
-    const [config, setConfig] = useState(movieConfig);
+export const ConfigurationMovie = forwardRef(
+    (
+        { configTitle, movieConfig, avaliableGenres, onSubmitClick }: Props,
+        ref: ForwardedRef<unknown>,
+    ) => {
+        const [config, setConfig] = useState(movieConfig);
 
-    const onKeyChangeHandle = (key: string, value: string | string[]) => {
-        setConfig({ ...config, [key]: value });
-    };
+        const onKeyChangeHandle = (key: string, value: string | string[]) => {
+            setConfig({ ...config, [key]: value });
+        };
 
-    const onSubmitHandle = () => {
-        onSubmitClick(config);
-    };
+        const onSubmitHandle = () => {
+            onSubmitClick(config);
+        };
 
-    const onResetHandle = () => {
-        setConfig({
-            ...config,
-            title: '',
-            release_date: '',
-            poster_path: '',
-            vote_average: 0,
-            genres: [],
-            runtime: 0,
-            overview: '',
-        });
-    };
+        const onResetHandle = () => {
+            setConfig({
+                ...config,
+                title: '',
+                release_date: '',
+                poster_path: '',
+                vote_average: 0,
+                genres: [],
+                runtime: 0,
+                overview: '',
+            });
+        };
 
-    const onHandleGenreChange = (event: SelectChangeEvent<typeof config.genres>) => {
-        const {
-            target: { value },
-        } = event;
-        setConfig({
-            ...config,
-            genres: typeof value === 'string' ? value.split(',') : value,
-        });
-    };
+        const onHandleGenreChange = (event: SelectChangeEvent<typeof config.genres>) => {
+            const {
+                target: { value },
+            } = event;
+            setConfig({
+                ...config,
+                genres: typeof value === 'string' ? value.split(',') : value,
+            });
+        };
 
-    const submitButtonDisabled = !requiredFildFilled(requiredFildsName, config);
+        const submitButtonDisabled = !requiredFildFilled(requiredFildsName, config);
 
-    return (
-        <ConfigurationMovieBox>
-            <Typography variant='h6' component='h2'>
-                {configTitle} MOVIE
-            </Typography>
-            <br />
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={8}>
-                    <Input
-                        label='Title'
-                        variant='outlined'
-                        style={configurationStyles.input}
-                        value={config.title}
-                        onChange={(e) =>
-                            onKeyChangeHandle(movieConfigNames.title, e.target.value)
-                        }
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <DatePicker
-                        label='Release date'
-                        value={config.release_date}
-                        onChange={(newValue) => {
-                            onKeyChangeHandle(
-                                movieConfigNames.release_date,
-                                formatDate(newValue),
-                            );
-                        }}
-                        renderInput={(params) => (
-                            <Input style={configurationStyles.input} {...params} />
-                        )}
-                    />
-                </Grid>
-                <Grid item xs={8}>
-                    <Input
-                        style={configurationStyles.input}
-                        label='Movie Url'
-                        variant='outlined'
-                        type='url'
-                        value={config.poster_path}
-                        onChange={(e) =>
-                            onKeyChangeHandle(
-                                movieConfigNames.poster_path,
-                                e.target.value,
-                            )
-                        }
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <Input
-                        style={configurationStyles.input}
-                        variant='outlined'
-                        type='number'
-                        label='Rating'
-                        value={config.vote_average}
-                        onChange={(e) =>
-                            onKeyChangeHandle(
-                                movieConfigNames.vote_average,
-                                e.target.value,
-                            )
-                        }
-                    />
-                </Grid>
-                <Grid item xs={8}>
-                    <FormControl sx={{ width: '100%' }}>
-                        <InputLabel id='genres-checkbox-label'>Genres</InputLabel>
-                        <Select
-                            labelId='genres-checkbox-label'
-                            multiple
-                            value={config.genres}
-                            onChange={onHandleGenreChange}
-                            input={<OutlinedInput label='Genres' />}
-                            renderValue={(selected) => selected.join(', ')}
-                            MenuProps={MenuProps}
+        return (
+            <ConfigurationMovieBox>
+                <Typography variant='h6' component='h2'>
+                    {configTitle} MOVIE
+                </Typography>
+                <br />
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid item xs={8}>
+                        <Input
+                            ref={ref}
+                            label='Title'
+                            variant='outlined'
+                            style={configurationStyles.input}
+                            value={config.title}
+                            onChange={(e) =>
+                                onKeyChangeHandle(movieConfigNames.title, e.target.value)
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <DatePicker
+                            label='Release date'
+                            value={config.release_date}
+                            onChange={(newValue) => {
+                                onKeyChangeHandle(
+                                    movieConfigNames.release_date,
+                                    formatDate(newValue),
+                                );
+                            }}
+                            renderInput={(params) => (
+                                <Input style={configurationStyles.input} {...params} />
+                            )}
+                        />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Input
+                            style={configurationStyles.input}
+                            label='Movie Url'
+                            variant='outlined'
+                            type='url'
+                            value={config.poster_path}
+                            onChange={(e) =>
+                                onKeyChangeHandle(
+                                    movieConfigNames.poster_path,
+                                    e.target.value,
+                                )
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Input
+                            style={configurationStyles.input}
+                            variant='outlined'
+                            type='number'
+                            label='Rating'
+                            value={config.vote_average}
+                            onChange={(e) =>
+                                onKeyChangeHandle(
+                                    movieConfigNames.vote_average,
+                                    e.target.value,
+                                )
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <FormControl sx={{ width: '100%' }}>
+                            <InputLabel id='genres-checkbox-label'>Genres</InputLabel>
+                            <Select
+                                labelId='genres-checkbox-label'
+                                multiple
+                                value={config.genres}
+                                onChange={onHandleGenreChange}
+                                input={<OutlinedInput label='Genres' />}
+                                renderValue={(selected) => selected.join(', ')}
+                                MenuProps={MenuProps}
+                            >
+                                {avaliableGenres.map((genre, id) => (
+                                    <MenuItem key={id} value={genre}>
+                                        <Checkbox
+                                            checked={config.genres.indexOf(genre) > -1}
+                                        />
+                                        <ListItemText primary={genre} />
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Input
+                            style={configurationStyles.input}
+                            variant='outlined'
+                            type='number'
+                            label='Runtime'
+                            value={config.runtime}
+                            onChange={(e) =>
+                                onKeyChangeHandle(
+                                    movieConfigNames.runtime,
+                                    e.target.value,
+                                )
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Input
+                            multiline
+                            style={{ ...configurationStyles.input, height: '100%' }}
+                            minRows={3}
+                            maxRows={6}
+                            label='Overview'
+                            value={config.overview}
+                            onChange={(e) =>
+                                onKeyChangeHandle(
+                                    movieConfigNames.overview,
+                                    e.target.value,
+                                )
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={6} />
+
+                    <Grid item xs={3}>
+                        <ResetButton variant='outlined' onClick={onResetHandle}>
+                            RESET
+                        </ResetButton>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <SubmitButton
+                            variant='contained'
+                            endIcon={<SendIcon />}
+                            onClick={onSubmitHandle}
+                            disabled={submitButtonDisabled}
                         >
-                            {avaliableGenres.map((genre, id) => (
-                                <MenuItem key={id} value={genre}>
-                                    <Checkbox
-                                        checked={config.genres.indexOf(genre) > -1}
-                                    />
-                                    <ListItemText primary={genre} />
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
+                            SUBMIT
+                        </SubmitButton>
+                    </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                    <Input
-                        style={configurationStyles.input}
-                        variant='outlined'
-                        type='number'
-                        label='Runtime'
-                        value={config.runtime}
-                        onChange={(e) =>
-                            onKeyChangeHandle(movieConfigNames.runtime, e.target.value)
-                        }
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <Input
-                        multiline
-                        style={{ ...configurationStyles.input, height: '100%' }}
-                        minRows={3}
-                        maxRows={6}
-                        label='Overview'
-                        value={config.overview}
-                        onChange={(e) =>
-                            onKeyChangeHandle(movieConfigNames.overview, e.target.value)
-                        }
-                    />
-                </Grid>
-                <Grid item xs={6} />
-
-                <Grid item xs={3}>
-                    <ResetButton variant='outlined' onClick={onResetHandle}>
-                        RESET
-                    </ResetButton>
-                </Grid>
-                <Grid item xs={3}>
-                    <SubmitButton
-                        variant='contained'
-                        endIcon={<SendIcon />}
-                        onClick={onSubmitHandle}
-                        disabled={submitButtonDisabled}
-                    >
-                        SUBMIT
-                    </SubmitButton>
-                </Grid>
-            </Grid>
-        </ConfigurationMovieBox>
-    );
-};
+            </ConfigurationMovieBox>
+        );
+    },
+);
